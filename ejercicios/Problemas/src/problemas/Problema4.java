083ede8d7b2d7364a3597be90fb75e583b4349f0
“raul8005” <rimedina@utpl.edu.ec>
2023-01-06 22:29:43 -0500

    Taller 11

--- ejercicios/Problemas/src/problemas/Problema4.java
+++ ejercicios/Problemas/src/problemas/Problema4.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,62 @@ public class Problema4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        int cedula;
+        String nombre;
+        boolean bandera = true;
+        while (bandera) {
+            System.out.println("Ingrese su nombre por favor");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese su cedula por favor");
+            cedula = entrada.nextInt();
+            System.out.println("Ingrese \n1 para obtener el valor de la "
+                    + "plantilla de luz\n2 para obtener el predio del "
+                    + "inmueble");
+            opcion = entrada.nextInt();
+
+            if (opcion == 1) {
+                calcularValorLuz(nombre, cedula);
+            } else {
+                if (opcion == 2) {
+                    calcularPredio(nombre, cedula);
+                }
+            }
+
+            System.out.println("Ingrese cualquier numero para salir");
+            opcion = entrada.nextInt();
+            if (opcion != 1 && opcion != 2) {
+                bandera = false;
+            }
+        }
     }
-    
+
+    public static void calcularValorLuz(String nombre, int cedula) {
+        Scanner entrada = new Scanner(System.in);
+        double valorkilovatio;
+        double kilovatio;
+        double operacion;
+        System.out.println("Ingrese el valor del kilovatio");
+        valorkilovatio = entrada.nextDouble();
+        System.out.println("Ingrese los kilovatios consumidos");
+        kilovatio = entrada.nextDouble();
+
+        operacion = valorkilovatio * kilovatio;
+        System.out.printf("Cliente %s con cedula %s debe cancelar el valor"
+                + "de $%.0f", nombre, cedula, operacion);
+
+    }
+
+    public static void calcularPredio(String nombre, int cedula) {
+        Scanner entrada = new Scanner(System.in);
+        double valorInmueble;
+        double valorPredio;
+        System.out.println("Ingrese el valor del Inmueble");
+        valorInmueble = entrada.nextDouble();
+        valorPredio = valorInmueble * 0.02;
+        System.out.printf("Cliente %s con cedula %s tiene un bien"
+                + "inmueble valorado en $%.0f y tiene que pagar de predio $%.0f",
+                 nombre, cedula, valorInmueble, valorPredio);
+    }
 }
--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -9,6 +9,26 @@ package problemas;
  *
  * @author reroes
  */
+
 public class Problema5 {
-    
+    // SANTIAGO CHAVEZ Y RAUL MEDINA
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        double a = obtenerSuma(informacion);
+        System.out.printf("La suma del arreglo es: %.0f    ",a);
+        
+    }
+
+    public static double obtenerSuma(int [][] informacion) {
+        double suma = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+              suma = suma + informacion[i][j];  
+            }  
+        }
+        return suma;
+        
+    }
 }
+    
+
